656bb3dc858ec0d8dc21644ba63e1a45eec87d01
Vlad <53820373+catprogrammer-cat@users.noreply.github.com>
2019-11-04 14:20:36 +0200

    Update lab6_3.cpp

--- lab6_3.cpp
+++ lab6_3.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
 
 using namespace std;
-
+    
+    void array_show_odd_quantity(int quantity){
+        // int quantity = 14;
+    
+        int sum = 0;
+        int quantity_even = 0;
+        int array[quantity];
+    
+        for(int i = 0; i <= quantity; i++){
+            cout << "Enter " << i << " element ";
+            cin >> array[i];
+            if(array[i] % 2 == 0 && array[i] != 0){
+                sum += array[i];
+                quantity_even++;
+            }
+            
+        }
+        cout << "sum is " << sum << endl;
+        cout << "quantity of even elements is " << quantity_even;
+    }
 int main()
 {
     
-    int quantity = 14;
-
-    int sum = 0;
-    int quantity_even = 0;
-    int array[quantity];
-
-    for(int i = 0; i <= quantity; i++){
-        cout << "Enter " << i << " element ";
-        cin >> array[i];
-        if(array[i] % 2 == 0 && array[i] != 0){
-            sum += array[i];
-            quantity_even++;
-        }
-        
-    }
-    cout << "sum is " << sum << endl;
-    cout << "quantity of even elements is " << quantity_even;
+    
+    array_show_odd_quantity(5);
     return 0;
 }
 //input:  4 4 2 0 0 0 0 0 0 0 0 0 0 0 0 
